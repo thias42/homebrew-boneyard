f6f3f7d66362465608808ba7498c4ec984d8a00e
ilovezfs <ilovezfs@icloud.com>
2016-08-01 02:53:26 -0700

    kdebase-runtime: update url, sha256

--- kdebase-runtime.rb
+++ kdebase-runtime.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class KdebaseRuntime < Formula
   homepage "http://www.kde.org/"
-  url "ftp://ftp.kde.org/pub/kde/stable/4.6.0/src/kdebase-runtime-4.6.0.tar.bz2"
-  sha1 "024b7035a410f7c08e501ea1250553e746a469d2"
+  url "http://pkgs.fedoraproject.org/lookaside/pkgs/kdebase-runtime/kdebase-runtime-4.6.0.tar.bz2/1f9d6bc64d7b84a74dd3ab06615c71ce/kdebase-runtime-4.6.0.tar.bz2"
+  sha256 "072d442e5ee2bb976987093a498c667c0a4a1205d5ee1dde844b23718fa792ef"
 
   depends_on "cmake" => :build
   depends_on "kde-phonon"
