2cb67181d60e1c18ce92c8bd677f878d0887f8ae
ilovezfs <ilovezfs@icloud.com>
2016-08-01 02:58:23 -0700

    qimageblitz: update url, sha256

--- qimageblitz.rb
+++ qimageblitz.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Qimageblitz < Formula
   homepage "http://www.kde.org"
-  url "ftp://ftp.kde.org/pub/kde/stable/qimageblitz/qimageblitz-0.0.6.tar.bz2"
-  sha1 "32bcda9adb262ef49c62e3a7adbfcdeec98f72e4"
+  url "http://pkgs.fedoraproject.org/repo/pkgs/qimageblitz/qimageblitz-0.0.6.tar.bz2/0ae2f7d4e0876764a97ca73799f61df4/qimageblitz-0.0.6.tar.bz2"
+  sha256 "9f102269dec50641440e23a449df215a0db9efef9a3969939d618c5e78a5010f"
 
   depends_on "cmake" => :build
   depends_on "qt"
