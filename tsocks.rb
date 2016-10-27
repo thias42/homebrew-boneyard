9704c9f80b8ed10e1f7e62f81f14eb07f86bff68
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-27 22:54:58 +0200

    tsocks: migrate from head-only
    Closes #221.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

---
+++ tsocks.rb
@@ -0,0 +1,21 @@
+class Tsocks < Formula
+  # The original is http://tsocks.sourceforge.net/
+  # This GitHub repo is a maintained fork with OSX support
+  homepage "https://github.com/pc/tsocks"
+  head "https://github.com/pc/tsocks.git"
+
+  depends_on "autoconf" => :build
+
+  def install
+    system "autoconf", "-v"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-conf=#{etc}/tsocks.conf"
+
+    inreplace("tsocks") { |s| s.change_make_var! "LIBDIR", lib }
+
+    system "make"
+    system "make", "install"
+    etc.install "tsocks.conf.simple.example" => "tsocks.conf"
+  end
+end
