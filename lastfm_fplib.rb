7f2c7695ba04f1b16cc5946381e62a62ee75e325
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-27 22:53:51 +0200

    lastfm_fplib: migrate from head-only
    Closes #219.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

---
+++ lastfm_fplib.rb
@@ -0,0 +1,18 @@
+class LastfmFplib < Formula
+  homepage "http://blog.last.fm/2007/08/29/audio-fingerprinting-for-clean-metadata"
+  head "svn://svn.audioscrobbler.net/recommendation/MusicID/lastfm_fplib"
+
+  depends_on "cmake" => :build
+  depends_on "taglib"
+  depends_on "mad"
+  depends_on "libsamplerate"
+  depends_on "fftw"
+
+  def install
+    inreplace "fplib/src/FloatingAverage.h",
+      "for ( int i = 0; i < size; ++i )",
+      "for ( int i = 0; i < m_values.size(); ++i )"
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
