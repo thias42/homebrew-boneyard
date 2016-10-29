0451e3cd2b04a0eb0d8eb798cede143b6ebde101
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-29 23:41:50 +0200

    otx: migrate from head-only
    Closes #227.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

---
+++ otx.rb
@@ -0,0 +1,12 @@
+class Otx < Formula
+  homepage "https://github.com/darwin/otx"
+  head "https://github.com/darwin/otx.git"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "SYMROOT=build"
+    bin.install "build/Release/otx"
+    prefix.install "build/Release/otx.app"
+  end
+end
