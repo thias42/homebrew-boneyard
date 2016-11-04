74598385dbd319b9e8615c12f1e5780be01a2a23
ilovezfs <ilovezfs@icloud.com>
2016-11-04 08:53:14 -0700

    tevent: migrate from core
    Closes #238.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ tevent.rb
@@ -0,0 +1,17 @@
+class Tevent < Formula
+  desc "Event system based on the talloc memory management library"
+  homepage "https://tevent.samba.org/"
+  url "https://www.samba.org/ftp/tevent/tevent-0.9.21.tar.gz"
+  sha256 "f2be7463573dab2d8210cb57fe7e7e2aeb323274cbdc865a6e29ddcfb977f0f4"
+
+  depends_on "pkg-config" => :build
+  depends_on "talloc"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-rpath",
+                          "--without-gettext",
+                          "--bundled-libraries=!talloc"
+    system "make", "install"
+  end
+end
