97737dd0a309ac4ccdda1f8964e808fcb83a2306
ilovezfs <ilovezfs@icloud.com>
2016-11-04 10:44:56 -0700

    swish-e: migrate from core
    Closes #239.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

---
+++ swish-e.rb
@@ -0,0 +1,19 @@
+class SwishE < Formula
+  desc "System for indexing collections of web pages"
+  homepage "http://swish-e.org/"
+  url "http://swish-e.org/distribution/swish-e-2.4.7.tar.gz"
+  sha256 "5ddd541ff8ecb3c78ad6ca76c79e620f457fac9f7d0721ad87e9fa22fe997962"
+
+  depends_on "libxml2"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"swish-e", "-S", "fs", "-i", *Dir[HOMEBREW_PREFIX/"*.md"]
+    output = shell_output("#{bin}/swish-e -w respect")
+    assert_match /^# Number of hits: [1-9]\d*$/, output
+  end
+end
