8549b18d32b40e070837c3c7b3db7d0b440b2d23
ilovezfs <ilovezfs@icloud.com>
2017-02-24 10:32:28 -0800

    silc-client: migrate from core (#289)

---
+++ silc-client.rb
@@ -0,0 +1,29 @@
+class SilcClient < Formula
+  desc "SILC conferencing client"
+  homepage "http://silcnet.org/client.html"
+  url "https://downloads.sourceforge.net/project/silc/silc/client/sources/silc-client-1.1.11.tar.gz"
+  sha256 "8cedf2f3c15322296afe094de60504bc27e349f1942713a2f322c7ef6ad5089e"
+  revision 1
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "openssl"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--with-perl=no",
+                          "--enable-ssl",
+                          "--enable-ipv6",
+                          "--with-socks",
+                          "--disable-asm"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/silc", "-v"
+  end
+end
