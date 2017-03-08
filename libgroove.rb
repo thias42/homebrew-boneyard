e03f68778f880c1be032de60c761bc7109eb8280
Dominyk Tiller <DomT4@users.noreply.github.com>
2017-03-08 12:26:25 +0000

    libgroove: migrate from core (#295)

---
+++ libgroove.rb
@@ -0,0 +1,32 @@
+class Libgroove < Formula
+  desc "Streaming audio processing library"
+  homepage "https://github.com/andrewrk/libgroove"
+  url "https://github.com/andrewrk/libgroove/archive/4.3.0.tar.gz"
+  sha256 "76f68896f078a9613d420339ef887ca8293884ad2cd0fbc031d89a6af2993636"
+  revision 1
+
+  depends_on :macos => :mavericks
+  depends_on "cmake" => :build
+  depends_on "ffmpeg" => "with-libvorbis"
+  depends_on "chromaprint"
+  depends_on "libebur128"
+  depends_on "sdl2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <groove/groove.h>
+      int main() {
+        groove_init();
+        groove_version();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-lgroove", "-o", "test"
+    system "./test"
+  end
+end
